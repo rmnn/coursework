a883df5eab1726ffd94dcae07f75ad8b51597d22
rmnn <dnsjkee@gmail.com>
2013-05-13 15:59:32 +0400

    add beautiful listview

--- game/src/ru/spbu/ageevd/ShowRatingActivity.java
+++ game/src/ru/spbu/ageevd/ShowRatingActivity.java
@@ -1,5 +1,8 @@
 package ru.spbu.ageevd;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import android.app.Activity;
 import android.database.Cursor;
 import android.os.Bundle;
@@ -8,8 +11,14 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class ShowRatingActivity extends Activity {
+	
+	private ArrayList <HashMap<String, Object>> myUsers;
+    private static final String USER_KEY = "userName";
+    private static final String RATING_KEY = "userRating";
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,36 +26,47 @@ public class ShowRatingActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_ratings);
-		ListView lv = (ListView) findViewById(R.id.listView1);
+		ListView listView = (ListView) findViewById(R.id.list);
+		
+		myUsers = new ArrayList<HashMap<String,Object>>();
+        HashMap<String, Object> hm;
+		
+		hm = new HashMap<String, Object>();
+        hm.put(USER_KEY, "Коробке");
+        hm.put(RATING_KEY, "какой-то текст");
+        
+        
 		DBAdapter db = new DBAdapter(this);
 		db.open();
-		String[] users = new String[1000];
-		int cnt = 0;
-		Log.d("ASD", "tut rabotaem");
 		Cursor cursor = db.getTopUsers();
 		Log.d("ASD", "tut eshe  rabotaem");
 		if (cursor.moveToFirst()) {	
-			Log.d("ASD", "tut ne zaidem");
-			users[cnt] = cursor.getString(1);
-			Log.d("ASD", "tut uje upali");
-			cnt++;
+			hm = new HashMap<String, Object>();
+	        hm.put(USER_KEY, cursor.getString(1));
+	        hm.put(RATING_KEY, cursor.getString(3));
+	        myUsers.add(hm);
 		}
 		while (cursor.moveToNext()) {
-			Log.d("ASD", "tut uje upali09090i");
-			users[cnt] = cursor.getString(1);
-			cnt++;
+			hm = new HashMap<String, Object>();
+	        hm.put(USER_KEY, cursor.getString(1));
+	        hm.put(RATING_KEY, cursor.getString(3));
+	        myUsers.add(hm);
 		}
-		Log.d("ASD", "vyshli iz while:) " + users[0]);
 		
-	    String[] catnames = new String[cnt - 1];
-		for (int i = 0; i < cnt - 1; i++) {
-			catnames[i] = users[i];
-		}
+		 SimpleAdapter adapter = new SimpleAdapter(this, 
+                 myUsers, 
+                 R.layout.activity_result, new String[]{
+                 USER_KEY,         //верхний текст
+                 RATING_KEY,        //нижний теккт
+                 }, new int[]{
+                 R.id.text1, //ссылка на объект отображающий текст
+                 R.id.text2, //ссылка на объект отображающий текст
+                       }); //добавили ссылку в чем отображать картинки из list.xml
 
-		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_list_item_1, catnames);
-
-		lv.setAdapter(adapter);
+          listView.setAdapter(adapter);
+       listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE); 
+		
+	   
 	}
 
 }
